8fc969d1ce7a8b192871003aa16b7000192502e0
PomHaffs <tomashaffenden@icloud.com>
2016-12-13 18:15:12 +1100

    Added logic and UI

--- TimesTable/ViewController.swift
+++ TimesTable/ViewController.swift
@@ -8,10 +8,53 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var currentNumber: UILabel!
+    @IBOutlet weak var table: UITableView!
+    
+    @IBOutlet weak var sliderSelection: UISlider!
+    
+    var selectedNumber = 0
+    
+    @IBAction func sliderMoved(_ sender: Any) {
+        
+        selectedNumber = Int(sliderSelection.value)
+        currentNumber.text = String(selectedNumber)
+        
+        if selectedNumber == 0 {
+            table.isHidden = true
+        } else {
+            table.isHidden = false
+        }
+        
+        table.reloadData()
+    }
+    
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 50
+    }
+    
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
+        
+        let answer = String(selectedNumber * indexPath.row)
+        
+        cell.textLabel?.text = "\(selectedNumber) multiplied by \(indexPath.row) equals \(answer)"
+    
+        return cell
+        
+    }
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
